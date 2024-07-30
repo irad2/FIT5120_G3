8b06b0510cb7f749e508f83d4b75ed9b1684ec9d
ashpoolresearch <research@ashpoolresearch.com>
2024-07-30 16:33:20 +1000

    init

---
+++ onboarding/public/script.js
@@ -0,0 +1,6 @@
+var map = L.map('map').setView([-37.8136, 144.9631], 13);
+
+L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
+    maxZoom: 19,
+    attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
+}).addTo(map);
