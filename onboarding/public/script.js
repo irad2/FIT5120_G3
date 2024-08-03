1ff90891fc3262b4df9680863870bf818beabc6f
irad2 <irad2@student.monash.edu>
2024-08-03 19:05:33 +1000

    directory structure simplified

--- onboarding/public/script.js
+++
@@ -1,6 +0,0 @@
-var map = L.map('map').setView([-37.8136, 144.9631], 13);
-
-L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
-    maxZoom: 19,
-    attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
-}).addTo(map);
