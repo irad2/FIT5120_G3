d61b7a2abf3634fa2ed0e49cf8ce28915ae0d5d9
irad2 <irad2@student.monash.edu>
2024-08-05 21:30:42 +1000

    haowei's work

--- onboarding/public/js/mapscript.js
+++ onboarding/public/js/mapscript.js
@@ -5,10 +5,11 @@ function loadGoogleMapsScript() {
     script.src = `https://maps.googleapis.com/maps/api/js?key=${config.googleMapsApiKey}&libraries=places&callback=initMap`;
     script.async = true;
     script.defer = true;
-  }
+}
 loadGoogleMapsScript();
 
 var map, directionsService, directionsRenderer, autocompleteStart, autocompleteEnd, unsafeZones = [];
+
 function initMap() {
     const map = new google.maps.Map(document.getElementById("map"), {
         center: { lat: -37.81, lng: 144.96 },
@@ -18,109 +19,120 @@ function initMap() {
     bikeLayer.setMap(map);
     console.log("Map initialized");
 
-        const directionsService = new google.maps.DirectionsService();
-        const directionsRenderer = new google.maps.DirectionsRenderer();
-        directionsRenderer.setMap(map);
-        fetchUnsafeZones().then(data => {
-            displayUnsafeZones(data, map);
+    const directionsService = new google.maps.DirectionsService();
+    const directionsRenderer = new google.maps.DirectionsRenderer();
+    directionsRenderer.setMap(map);
+    fetchUnsafeZones().then(data => {
+        displayUnsafeZones(data, map);
+    });
+
+    setupDirections(directionsService, directionsRenderer);
+
+
+}
+
+async function fetchUnsafeZones() {
+    try {
+        const response = await fetch('/data/most_dangerous.json');
+        return await response.json();
+    } catch (error) {
+        console.error('Failed to fetch unsafe zones:', error);
+        return [];
+    }
+}
+
+
+function displayUnsafeZones(data, map) {
+    var infoWindow = new google.maps.InfoWindow();
+
+    data.forEach(zone => {
+        var zonePosition = { lat: zone.Latitude, lng: zone.Longitude };
+        var marker = new google.maps.Marker({
+            position: zonePosition,
+            map: map,
+            title: "Unsafe Zone: " + zone.Road,
+            icon: {
+                url: "images/risk-icon.svg",
+                scaledSize: new google.maps.Size(20, 20),
+                anchor: new google.maps.Point(10, 10)
+            }
         });
 
-        setupDirections(directionsService, directionsRenderer);
-
-        
-    }
-    
-    async function fetchUnsafeZones() {
-        try {
-            const response = await fetch('/data/most_dangerous.json');
-            return await response.json();
-        } catch (error) {
-            console.error('Failed to fetch unsafe zones:', error);
-            return [];
-        }
-    }
-
-     
-    function displayUnsafeZones(data, map) {
-        data.forEach(zone => {
-            var zonePosition = {lat: zone.Latitude, lng: zone.Longitude};
-            var marker = new google.maps.Marker({
-                position: zonePosition,
-                map: map,
-                title: "Unsafe Zone: " + zone.Road,
-                icon: {
-                    url: "images/risk-icon.svg",
-                    scaledSize: new google.maps.Size(20, 20),
-                    anchor: new google.maps.Point(10, 11)    
-                }
-            });
-
-
-    
-            var circle = new google.maps.Circle({
-                strokeColor: '#FF0000',
-                strokeOpacity: 0.8,
-                strokeWeight: 2,
-                fillColor: '#FF0000',
-                fillOpacity: 0.35,
-                map,
-                center: zonePosition,
-                radius: 500,  // radius should be adjusted based on your specific needs,
-                label: "Unsafe Zone: " + zone.Road
-            });
-
-            console.log("Unsafe zone added: " + zone.Latitude + ", " + zone.Longitude);
-    
-            // Store the circle object for later use in routing checks
-            unsafeZones.push(circle);
-        });
-    }
-
-    function setupDirections(directionsService, directionsRenderer) {
-        const originInput = document.getElementById("startingInput");
-        const destinationInput = document.getElementById("destinationInput");
-        const findDirectionsButton = document.getElementById("search_btn");
-
-        const originAutocomplete = new google.maps.places.Autocomplete(originInput);
-        originAutocomplete.setComponentRestrictions({ country: ["au"] });
-        const destinationAutocomplete = new google.maps.places.Autocomplete(destinationInput);
-        destinationAutocomplete.setComponentRestrictions({ country: ["au"] });
-    
-        findDirectionsButton.addEventListener("click", () => {
-            const origin = originInput.value;
-            const destination = destinationInput.value;
-            console.log("Finding directions from " + origin + " to " + destination);
-    
-            directionsService.route(
-                {
-                    origin: origin,
-                    destination: destination,
-                    travelMode: google.maps.TravelMode.BICYCLING,
-                },
-                (response, status) => {
-                    if (status === "OK") {
-                        var routePath = response.routes[0].overview_path;
-                        if (!checkRouteSafety(routePath)) {
-                            
-                            console.log("Directions found");
-                        } else {
-                            alert('Your route passes through an unsafe area. Please choose a different route or be careful');
-                        }
-                        directionsRenderer.setDirections(response);
-                    } else {
-                        console.error("Directions request failed due to " + status);
-                    }
-                }
+        marker.addListener('click', function() {
+            infoWindow.setContent('<strong>' + zone.Road + '</strong><br>' +
+                'Issues Raised: ' + zone['Issues Raised'] + '</div>'
             );
+
+            infoWindow.open(map, marker);
         });
-    }
-    function checkRouteSafety(routePath) {
-        return routePath.some(pathPoint => {
-            return unsafeZones.some(zone => {
-                return google.maps.geometry.spherical.computeDistanceBetween(pathPoint, zone.getCenter()) < zone.getRadius();
-            });
+
+
+
+
+        var circle = new google.maps.Circle({
+            strokeColor: '#FF0000',
+            strokeOpacity: 0.8,
+            strokeWeight: 2,
+            fillColor: '#FF0000',
+            fillOpacity: 0.35,
+            map,
+            center: zonePosition,
+            radius: 500, // radius should be adjusted based on your specific needs,
+            label: "Unsafe Zone: " + zone.Road
         });
-    }
+
+        console.log("Unsafe zone added: " + zone.Latitude + ", " + zone.Longitude);
+
+        // Store the circle object for later use in routing checks
+        unsafeZones.push(circle);
+    });
+}
+
+function setupDirections(directionsService, directionsRenderer) {
+    const originInput = document.getElementById("startingInput");
+    const destinationInput = document.getElementById("destinationInput");
+    const findDirectionsButton = document.getElementById("search_btn");
+
+    const originAutocomplete = new google.maps.places.Autocomplete(originInput);
+    originAutocomplete.setComponentRestrictions({ country: ["au"] });
+    const destinationAutocomplete = new google.maps.places.Autocomplete(destinationInput);
+    destinationAutocomplete.setComponentRestrictions({ country: ["au"] });
+
+    findDirectionsButton.addEventListener("click", () => {
+        const origin = originInput.value;
+        const destination = destinationInput.value;
+        console.log("Finding directions from " + origin + " to " + destination);
+
+        directionsService.route({
+                origin: origin,
+                destination: destination,
+                travelMode: google.maps.TravelMode.BICYCLING,
+            },
+            (response, status) => {
+                if (status === "OK") {
+                    var routePath = response.routes[0].overview_path;
+                    if (!checkRouteSafety(routePath)) {
+
+                        console.log("Directions found");
+                    } else {
+                        alert('Your route passes through an unsafe area. Please choose a different route or be careful');
+                    }
+                    directionsRenderer.setDirections(response);
+                } else {
+                    console.error("Directions request failed due to " + status);
+                }
+            }
+        );
+    });
+}
+
+function checkRouteSafety(routePath) {
+    return routePath.some(pathPoint => {
+        return unsafeZones.some(zone => {
+            return google.maps.geometry.spherical.computeDistanceBetween(pathPoint, zone.getCenter()) < zone.getRadius();
+        });
+    });
+}
 
 
 window.onload = function() {
