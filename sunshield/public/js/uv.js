6d79744625443c9ee6e21c4cb7c583fe0539f5cc
hlin0067 <hlin0067@student.monash.edu>
2024-08-28 22:42:34 +1000

    uv page modify

--- sunshield/public/js/uv.js
+++ sunshield/public/js/uv.js
@@ -1,4 +1,4 @@
-const apiKey = "API_KEY";
+const apiKey = "";
 
 
 
@@ -66,7 +66,7 @@ document.addEventListener("DOMContentLoaded", () => {
                 const date = new Date(timestamp * 1000);
                 const today = new Date();
                 let formattedDate;
-                
+
                 if (date.toDateString() === today.toDateString()) {
                     formattedDate = 'Today';
                 } else {
@@ -96,7 +96,7 @@ document.addEventListener("DOMContentLoaded", () => {
                         textColor = "purple";
                     }
                 }
-                
+
                 const dateElement = document.getElementById(`uv-index-${index}-date`);
                 const uvIndexTextElement = document.getElementById(`uv-index-${index}-text`);
                 const uvIndexUVIElement = document.getElementById(`uv-index-${index}-uvi`);
@@ -115,77 +115,7 @@ document.addEventListener("DOMContentLoaded", () => {
         });
     }
 
-    async function displayUVIndexes(targetTimes) {
-        try {
-            const { lat, lon } = await getGeolocation();
-            const uvIndexes = await getUVIndexes(lat, lon, targetTimes);
-            const indexDict = {}
-            targetTimes.forEach((time, index) => {
-                indexDict[time] = uvIndexes[index];
-            });
-            console.log("indexDict:", indexDict);
-            updateUVIndexes(indexDict);
-        } catch (error) {
-            console.error(error);
-            updateUVIndexes(new Array(targetTimes.length).fill(null));
-        }
-    }
 
-    function switchBackground(element, uvIndexText) {
-        switch(uvIndexText) {
-            case "Low":
-                element.style.backgroundImage = "url('Resource/bg_uv_green.png')";
-                break;
-            case "Moderate":
-                element.style.backgroundImage = "url('Resource/bg_uv_yellow.png')";
-                break;
-            case "High":
-                element.style.backgroundImage = "url('Resource/bg_uv_orange.png')";
-                break;
-            case "Very High":
-                element.style.backgroundImage = "url('Resource/bg_uv_red.png')";
-                break;
-            case "Extreme":
-                element.style.backgroundImage = "url('Resource/bg_uv_purple.png')";
-                break;
-            default:
-                element.style.backgroundImage = "url('Resource/bg_uv_green.png')";
-                break;
-        }
-    }
-
-    function getNextWeekDates() {
-        var dates = [];
-        var today = new Date();
-
-        for (var i = 0; i < 7; i++) {
-            var nextDay = new Date(today);
-            nextDay.setDate(today.getDate() + i);
-            dates.push(nextDay);
-        }
-
-        return dates;
-    }
-
-    var weekDates = getNextWeekDates();
-    weekDates.forEach(date => console.log(date.toDateString()));
-
-    function getNextWeekNoonTimestamps() {
-        var timestamps = [];
-        for (var i = 0; i < 7; i++) {
-            var date = new Date();
-            date.setDate(date.getDate() + i);
-            date.setHours(12, 0, 0, 0);
-            timestamps.push(Math.floor(date.getTime() / 1000));
-        }
-        return timestamps;
-    }
-    var nextWeekNoonTimestamps = getNextWeekNoonTimestamps();
-    console.log(nextWeekNoonTimestamps);
-
-
-
-    displayUVIndexes(nextWeekNoonTimestamps);
 
     // Function to get the current UV index
     async function getCurrentUVIndex() {
@@ -208,14 +138,14 @@ document.addEventListener("DOMContentLoaded", () => {
             if (currentUVElement) {
                 currentUVElement.innerHTML = `
                 <p>Current UV Index in:</p>
-                <p>_</p>
-                <p>Loading...</p>
+                <p>_:Loading...</p>
+
                 `;
             }
             const { lat, lon } = await getGeolocation();
             const currentUVIndex = await getCurrentUVIndex();
             console.log(lat, lon, currentUVIndex);
-            
+
             if (currentUVElement) {
                 if (currentUVIndex !== null) {
                     const locationName = await getLocationName(lat, lon);
@@ -225,44 +155,46 @@ document.addEventListener("DOMContentLoaded", () => {
                     }
                     currentUVElement.innerHTML = `
                         <p>Current UV Index in</p>
-                        <p>${locationName}:</p>
-                        <p>${currentUVIndex.toFixed(1)}</p>
+                        <p>${locationName}:${currentUVIndex.toFixed(1)}</p>
                     `;
                     const locationNameElement = document.getElementById('location-name');
                     if (locationNameElement) {
                         locationNameElement.textContent = locationName;
                     }
                 } else {
-                    currentUVElement.textContent = 'Current UV Index: Unavailable';
+                    currentUVElement.innerHTML = `
+                    </p>Current UV Index:</p>
+                    <p> Unavailable<p>
+                    `;
                 }
             }
             const currentDateElement = document.getElementById('current-date');
             if (currentDateElement) {
                 currentDateElement.textContent = new Date().toLocaleDateString('en-US', { weekday: 'long', month: 'long', day: 'numeric' });
-                
+
             }
             const currentDateMain = document.getElementById('current-date-main');
-                if (currentDateMain) {
-                    currentDateMain.textContent = new Date().toLocaleDateString('en-US', { weekday: 'long', month: 'long', day: 'numeric' });
-                }
+            if (currentDateMain) {
+                currentDateMain.textContent = new Date().toLocaleDateString('en-US', { weekday: 'long', month: 'long', day: 'numeric' });
+            }
             var uvIndexText = "";
             var uvIndexColor = "";
             if (currentUVIndex !== null) {
                 if (currentUVIndex < 3) {
                     uvIndexText = "Low";
-                    uvIndexColor = "green"; 
+                    uvIndexColor = "green";
                 } else if (currentUVIndex < 6) {
                     uvIndexText = "Moderate";
-                    uvIndexColor = "yellow"; 
+                    uvIndexColor = "yellow";
                 } else if (currentUVIndex < 8) {
                     uvIndexText = "High";
-                    uvIndexColor = "orange"; 
+                    uvIndexColor = "orange";
                 } else if (currentUVIndex < 11) {
                     uvIndexText = "Very High";
-                    uvIndexColor = "red"; 
+                    uvIndexColor = "red";
                 } else {
                     uvIndexText = "Extreme";
-                    uvIndexColor = "purple"; 
+                    uvIndexColor = "purple";
                 }
             }
 
@@ -270,11 +202,11 @@ document.addEventListener("DOMContentLoaded", () => {
             if (currentUVIndex2Element) {
                 const currentUVIndex2UVIElement = document.getElementById('current-uv-index2-uvi');
                 currentUVIndex2UVIElement.innerHTML = `${currentUVIndex.toFixed(1)} `;
-                currentUVIndex2UVIElement.style.color = uvIndexColor; 
+                currentUVIndex2UVIElement.style.color = uvIndexColor;
                 const uvIndexTextElement = document.getElementById('current-uv-index-text');
                 if (uvIndexTextElement) {
                     uvIndexTextElement.textContent = uvIndexText;
-                    
+
                 }
                 switchBackground(currentUVIndex2Element, uvIndexText);
             }
@@ -282,19 +214,206 @@ document.addEventListener("DOMContentLoaded", () => {
             console.error('Error displaying current UV index:', error);
             const currentUVElement = document.getElementById('current-uv-index');
             if (currentUVElement) {
-                currentUVElement.textContent = 'Unable to fetch location and UV index';
+                currentUVElement.innerHTML = `
+                <p>Unable to fetch location</p> 
+                <p>and UV index</p>
+                `;
             }
         }
     }
+
+    async function displayCurrentUVI() {
+        try {
+            const currentUVElement = document.getElementById('cur-uv');
+            const levelElement = document.getElementById('box-level-1');
+            const boxElement = document.getElementById('current-uv-box');
+            const numElement = document.getElementById('num-1');
+
+            if (!currentUVElement || !levelElement || !boxElement || !numElement) {
+                console.error('UI element for displaying current UV index or level not found');
+                return;
+            }
+            const currentUVIndex = await getCurrentUVIndex();
+            if (currentUVIndex !== null) {
+                currentUVElement.textContent = currentUVIndex.toFixed(1);
+                let bgImagePath;
+                let textColor;
+                if (currentUVIndex < 3) {
+                    levelElement.textContent = 'Low';
+                    bgImagePath = "Resource/bg_uv_green.png";
+                    textColor = "#3EA72D";
+                } else if (currentUVIndex < 6) {
+                    levelElement.textContent = 'Moderate';
+                    bgImagePath = "Resource/bg_uv_yellow.png";
+                    textColor = "#FFF300";
+                } else if (currentUVIndex < 8) {
+                    levelElement.textContent = 'High';
+                    bgImagePath = "Resource/bg_uv_orange.png";
+                    textColor = "#F18B00";
+                } else if (currentUVIndex < 11) {
+                    levelElement.textContent = 'Very High';
+                    bgImagePath = "Resource/bg_uv_red.png";
+                    textColor = "#E53210";
+                } else {
+                    levelElement.textContent = 'Extreme';
+                    bgImagePath = "Resource/bg_uv_purple.png";
+                    textColor = "#B567A4";
+                }
+                boxElement.style.backgroundImage = `url('${bgImagePath}')`;
+                currentUVElement.style.color = textColor;
+            } else {
+                currentUVElement.textContent = 'NA';
+                levelElement.textContent = 'NA';
+                boxElement.style.backgroundImage = "url('Resource/bg_uv_green.png')";
+                currentUVElement.style.color = "#000000";
+            }
+        } catch (error) {
+            console.error('Error displaying current UV index:', error);
+        }
+    }
+
+    async function displayTodaysUV() {
+        try {
+            const { lat, lon } = await getGeolocation();
+            const tdUVElement = document.getElementById('td-uv');
+            const tdLevelElement = document.getElementById('box-level-td');
+            const tdBoxElement = document.getElementById('td-uv-box');
+
+            if (!tdUVElement || !tdLevelElement || !tdBoxElement) {
+                console.error('UI element for displaying today\'s UV not found');
+                return;
+            }
+
+            const today = new Date();
+            today.setHours(0, 0, 0, 0);
+
+            const dailyUVIndex = await getUVIndexes(lat, lon, [today.getTime() / 1000]);
+
+            if (dailyUVIndex[0] === null) {
+                tdUVElement.textContent = 'NA';
+                tdLevelElement.textContent = 'NA';
+                tdBoxElement.style.backgroundImage = "url('Resource/bg_uv_green.png')";
+                tdUVElement.style.color = "#000000";
+            } else {
+                const uvIndex = dailyUVIndex[0];
+                tdUVElement.textContent = uvIndex.toFixed(1);
+                let bgImagePath;
+                let textColor;
+                if (uvIndex < 3) {
+                    tdLevelElement.textContent = 'Low';
+                    bgImagePath = "Resource/bg_uv_green.png";
+                    textColor = "#94d768";
+                } else if (uvIndex < 6) {
+                    tdLevelElement.textContent = 'Moderate';
+                    bgImagePath = "Resource/bg_uv_yellow.png";
+                    textColor = "#F6ca5c";
+                } else if (uvIndex < 8) {
+                    tdLevelElement.textContent = 'High';
+                    bgImagePath = "Resource/bg_uv_orange.png";
+                    textColor = "#f0965a";
+                } else if (uvIndex < 11) {
+                    tdLevelElement.textContent = 'Very High';
+                    bgImagePath = "Resource/bg_uv_red.png";
+                    textColor = "#eb473d";
+                } else {
+                    tdLevelElement.textContent = 'Extreme';
+                    bgImagePath = "Resource/bg_uv_purple.png";
+                    textColor = "#8455f6";
+                }
+                tdBoxElement.style.backgroundImage = `url('${bgImagePath}')`;
+                tdUVElement.style.color = textColor;
+            }
+        } catch (error) {
+            console.error('Error displaying today\'s UV data:', error);
+        }
+    }
+
+    function getNextThreeDays() {
+        let dates = [];
+        for (let i = 1; i <= 3; i++) {
+            let date = new Date();
+            date.setDate(date.getDate() + i);
+            dates.push(Math.floor(date.setHours(0, 0, 0, 0) / 1000));
+        }
+        return dates;
+    }
+    async function displayNextThreeDaysUV() {
+        try {
+            const { lat, lon } = await getGeolocation();
+            const nextDays = getNextThreeDays();
+            const uvIndexes = await getUVIndexes(lat, lon, nextDays);
+
+            uvIndexes.forEach((uvIndex, i) => {
+                const dayElement = document.getElementById(`day${i+1}-uv`);
+                const levelElement = document.getElementById(`box-level-d${i+1}`);
+                const boxElement = document.getElementById(`forecast-${i+1}`);
+                const dateElement = document.getElementById(`date${i+1}`);
+
+                if (!dayElement || !levelElement || !boxElement || !dateElement) {
+                    console.error(`UI element for displaying day ${i+1}'s UV not found`);
+                    return;
+                }
+
+                const date = new Date(nextDays[i] * 1000);
+                const options = { weekday: 'long', month: 'short', day: 'numeric' };
+                dateElement.textContent = date.toLocaleDateString('en-US', options);
+
+                if (uvIndex === null) {
+                    dayElement.textContent = 'NA';
+                    levelElement.textContent = 'NA';
+                    boxElement.style.backgroundImage = "url('Resource/bg_uv_green.png')";
+                    dayElement.style.color = "#000000";
+                } else {
+                    dayElement.textContent = uvIndex.toFixed(1);
+                    let bgImagePath;
+                    let textColor;
+
+
+                    if (uvIndex < 3) {
+                        levelElement.textContent = 'Low';
+                        bgImagePath = "Resource/bg_uv_green.png";
+                        textColor = "#94d768";
+                    } else if (uvIndex < 6) {
+                        levelElement.textContent = 'Moderate';
+                        bgImagePath = "Resource/bg_uv_yellow.png";
+                        textColor = "#F6ca5c";
+                    } else if (uvIndex < 8) {
+                        levelElement.textContent = 'High';
+                        bgImagePath = "Resource/bg_uv_orange.png";
+                        textColor = "#f0965a";
+                    } else if (uvIndex < 11) {
+                        levelElement.textContent = 'Very High';
+                        bgImagePath = "Resource/bg_uv_red.png";
+                        textColor = "#eb473d";
+                    } else {
+                        levelElement.textContent = 'Extreme';
+                        bgImagePath = "Resource/bg_uv_purple.png";
+                        textColor = "#8455f6";
+                    }
+                    boxElement.style.backgroundImage = `url('${bgImagePath}')`;
+                    dayElement.style.color = textColor;
+                }
+            });
+        } catch (error) {
+            console.error('Error displaying next three days UV data:', error);
+        }
+    }
+
+
+
+
+
     async function getLocationName(lat, lon) {
         const response = await fetch(`https://nominatim.openstreetmap.org/reverse?format=json&lat=${lat}&lon=${lon}`);
         const data = await response.json();
-        return data.address?.city || data.address?.town || data.address?.village || 'Unknown Location';
+        return data.address.city || data.address.town || data.address.village || 'Unknown Location';
     }
 
     // Call the function to display the current UV index
     displayCurrentUVIndex();
-
+    displayCurrentUVI();
+    displayTodaysUV();
+    displayNextThreeDaysUV();
     // Update the current UV index every 5 minutes
     setInterval(displayCurrentUVIndex, 5 * 60 * 1000);
 });
